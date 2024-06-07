79109a62386ca6263931a50118617aad29edfaba
byandell <byandell@tsakisti.doit.wisc.edu>
2024-06-07 15:08:43 -0500

    0.1.3 install explicit in app.R

--- R/foundrSetup.R
+++ R/foundrSetup.R
@@ -3,14 +3,12 @@
 #' @param data_instance type of data
 #' @param data_subset focus instance to selected dataset(s) if not `NULL` 
 #' @param custom_settings setup custom if `TRUE`
-#' @param install_packages install packages if `TRUE` (for deployment)
 #'
 #' @return invisible
 #' @export
 foundrSetup <- function(data_instance = c("Liver","Trait"),
                         data_subset = NULL,
-                        custom_settings = TRUE,
-                        install_packages = FALSE) {
+                        custom_settings = TRUE) {
   data_instance <- match.arg(data_instance)
   
   assign("data_instance", data_instance, envir = globalenv())
@@ -19,16 +17,5 @@ foundrSetup <- function(data_instance = c("Liver","Trait"),
   # Read trait data and set up custom settings.
   source(system.file(file.path("shinyApp", "TraitData.R"),
                      package = "foundrShiny"))
-  
-  if(install_packages) {
-    cat("Install packages for deployment.\n")
-    devtools::install_cran("plotly") #  not yet on UW dataviz
-    devtools::install_cran("markdown") #  not yet on UW dataviz
-    devtools::install_cran("cowplot") #  not yet on UW dataviz
-    devtools::install_cran("ggdendro") #  not yet on UW dataviz
-    devtools::install_github("byandell/foundr")
-    devtools::install_github("byandell/foundrShiny")
-    options(shiny.sanitize.errors = FALSE)
-  }
   invisible()
 }
--- inst/shinyApp/app.R
+++ inst/shinyApp/app.R
@@ -1,7 +1,14 @@
+devtools::install_cran("plotly") #  not yet on UW dataviz
+devtools::install_cran("markdown") #  not yet on UW dataviz
+devtools::install_cran("cowplot") #  not yet on UW dataviz
+devtools::install_cran("ggdendro") #  not yet on UW dataviz
+devtools::install_github("byandell/foundr")
+devtools::install_github("byandell/foundrShiny")
+options(shiny.sanitize.errors = FALSE)
+
 foundrShiny::foundrSetup(data_instance = "Liver",
                          data_subset = NULL,
-                         custom_settings = TRUE,
-                         install_packages = TRUE)
+                         custom_settings = TRUE)
 
 title <- "Founder Diet Study"
 
